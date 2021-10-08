82b03218b538adea665a11d6cb12841ea5b6924f
Zhang <504947985@qq.com>
2021-10-08 12:55:43 +0800

    完成代码相关功能

---
+++ setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="djangoshortcuts",
+    version="0.0.1",
+    author="Zhang",
+    description="Shortcuts to create files easily when use django",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "dj=djangoshortcuts.command:main",
+        ]
+    },
+    python_requires='>=3.5.2',
+)
